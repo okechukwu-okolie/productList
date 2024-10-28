1fa87771a137ac02fb78e48bf8df1a43600e305b
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2024-10-28 06:03:32 -0700

    responsiveness added

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,15 +1,21 @@
 const Header=()=>{
     return(
-        <div className="header">
-            <div>
-            <span className="easy">Easy</span><span className="buy">Buy</span>
-            </div>
-            <div>
-                <div className="input"><img src="./magnifying-glass-solid.svg" alt="" /><input type="text"  placeholder="Search for anything"/></div>
-            </div>
-            <div>
-                <img src="./cart-shopping-solid.svg" alt="" />
-            </div>
+        <div  className="header">
+                <div className='logo'>
+                    <span className="easy">Easy</span><span className="buy">Buy</span>
+                </div>
+        
+                <div className='input'>
+                    <img src="./magnifying-glass-solid.svg" alt="" />
+                    <input type="text"  placeholder="Search for anything"/>
+                </div>
+            
+                <div className='cart'>
+                    <img src="./cart-shopping-solid.svg" alt="" />
+                </div>
+                <div className='ham'>
+                    <img src="./hamburger.png" alt="" />
+                </div>
         </div>
     );
 }
