da89c0076d5886d6788637cac30506132281f6b4
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-09-30 16:36:38 +0100

    adding to the codebase

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,92 +1,156 @@
-import React, { createRef, useEffect, useState } from 'react'
+import React, { createRef, useEffect, useState } from "react";
+import { useFilter } from "./FilterContext";
 
-interface Product{
-  category:string;
+interface Product {
+  category: string;
 }
 
-interface FetchResponse{
-  products:Product[]
+interface FetchResponse {
+  products: Product[];
 }
-
-
 
 const Sidebar = () => {
-    const [categories, setCategories] = useState<string[]>([])
-    const [keywords] = useState<string[]>([
-        'apple',
-        'watch',
-        'fashion',
-        'trend',
-        'shoes',
-        'shirt',
-    ])
+  const {
+    searchQuery,
+    setSearchQuery,
+    selectedCategory,
+    setSelectedCategory,
+    minPrice,
+    setMinPrice,
+    maxPrice,
+    setMaxPrice,
+    keyword,
+    setKeyword,
+  } = useFilter();
 
-    useEffect(()=>{
-        const FetchCategory = async()=>{
-            try {
-                const response = await fetch('https://dummyjson.com/products')
-                const data:FetchResponse = await response.json()
-                
-                const uniqueCategories = Array.from(new Set(data.products.map((product)=>product.category)))
-                // console.log(uniqueCategories)
+  const [categories, setCategories] = useState<string[]>([]);
+  const [keywords] = useState<string[]>([
+    "apple",
+    "watch",
+    "fashion",
+    "trend",
+    "shoes",
+    "shirt",
+  ]);
 
-                setCategories(uniqueCategories)
-                
-            } catch (error) {
-                console.error('Error fetching product', error)
-            }
-        }
-        FetchCategory()
-    },[])
+  useEffect(() => {
+    const FetchCategory = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data: FetchResponse = await response.json();
+
+        const uniqueCategories = Array.from(
+          new Set(data.products.map((product) => product.category))
+        );
+        // console.log(uniqueCategories)
+
+        setCategories(uniqueCategories);
+      } catch (error) {
+        console.error("Error fetching product", error);
+      }
+    };
+    FetchCategory();
+  }, []);
+
+
+  const handleMinPriceChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    const value = e.target.value;
+    setMinPrice(value ? parseFloat(value):undefined)
+  }
+
+    const handleMaxPriceChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    const value = e.target.value;
+    setMaxPrice(value ? parseFloat(value):undefined)
+  }
+
+
+  const handleRadioChangeCategories = (category:string)=>{
+    setSelectedCategory(category)
+  }
+
+   const handleKeywordClick = (keyword:string)=>{
+
+   }
+
+
 
   return (
-    <div className='w-64 p-5 h-screen'>
+    <div className="w-64 p-5 h-screen">
       <h1 className="text-2xl font-bold mb-10 mt-4">React Store</h1>
 
-    <section>
-      <input type="text" className='border-[0.5px] rounded px-2 py-1 sm:mb-0' placeholder='Search Product' />
-
-      <div className="flex justify-center items-center">
-        <input type="text" className="border-2 mr-2 px-5 py-3 mb-3 w-full" placeholder='Min' />
-        <input type="text" className="border-2 mr-2 px-5 py-3 mb-3 w-full" placeholder='Max' />
-      </div>
-
-      {/* category section */}
       <section>
-        <div className="mb-5">
-        <h2 className="text-xl font-semibold mb-3">Categories</h2>
-      </div>
+        <input
+          type="text"
+          className="border-[0.5px] rounded px-2 py-1 sm:mb-0"
+          placeholder="Search Product"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
 
-      {categories.map((category,index)=>(
-        <label key={index} className='block mb-2 font-semibold text-[12px] cursor-pointer items-center flex'>
-          <input type="radio" name='category' value={category} className='mr-2 w-[16px] h-[16px] cursor-pointer' />
-          {category.toUpperCase()}
-        </label>
-      ))}
-
-      </section>
-
-      {/* keyword section */}
-
-      <div className="mb-5">
-        <h2 className="text-xl font-semibold mb-3 mt-4">Keywords</h2>
-
-        <div>
-          {keywords.map((keyword,index)=>(
-            <button key={index} className="block mb-2 px-4 py-2 w-full text-left border round hover:bg-gray-200 cursor-pointer">
-              {keyword.toUpperCase()}
-            </button>
-          ))}
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Min"
+            value={minPrice ?? ''}
+            onChange={handleMinPriceChange}
+          />
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Max"
+            value={maxPrice ?? ''}
+            onChange={handleMaxPriceChange}
+          />
         </div>
-      </div>
-<button className='w-full mb-[4rem] py-2 bg-black text-white rounded mt-5 cursor-pointer'>Reset Filter</button>
 
-    </section>
+        {/* category section */}
+        <section>
+          <div className="mb-5">
+            <h2 className="text-xl font-semibold mb-3">Categories</h2>
+          </div>
 
+          {categories.map((category, index) => (
+            <label
+              key={index}
+              className="block mb-2 font-semibold text-[12px] cursor-pointer items-center flex"
+            >
+              <input
+                type="radio"
+                name="category"
+                value={category}
+                onChange={()=>handleRadioChangeCategories(category)}
+                className="mr-2 w-[16px] h-[16px] cursor-pointer"
+                checked={selectedCategory === category}
+              />
+              {category.toUpperCase()}
+            </label>
+          ))}
+        </section>
 
+        {/* keyword section */}
 
+        <div className="mb-5">
+          <h2 className="text-xl font-semibold mb-3 mt-4">Keywords</h2>
+
+          <div>
+            {keywords.map((keyword, index) => (
+              <button
+                key={index}
+                className="block mb-2 px-4 py-2 w-full text-left border round hover:bg-gray-200 cursor-pointer"
+                onClick={()=> handleKeywordClick(keyword)}
+              >
+                {keyword.toUpperCase()}
+              </button>
+            ))}
+          </div>
+        </div>
+        <button className="w-full mb-[4rem] py-2 bg-black text-white rounded mt-5 cursor-pointer">
+          Reset Filter
+        </button>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { FilterProvider } from './components/FilterContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+  <StrictMode>
+    <FilterProvider>
+      <App />
+    </FilterProvider>
+  </StrictMode>
   
 )
